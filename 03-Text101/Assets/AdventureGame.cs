ab7c99d6b6cc942fca832f57daad6c313e6a8f13
Gabriel <gabrielstrzal@gmail.com>
2019-07-16 21:38:53 -0400

    addind Text101 game

---
+++ 03-Text101/Assets/AdventureGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] private Text textComponent;
+    [SerializeField] private State starterState;
+
+    private State currentState;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = starterState;
+        textComponent.text = currentState.GetStoryText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = currentState.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentState = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentState = nextStates[1];
+        }
+        textComponent.text = currentState.GetStoryText();
+    }
+}
