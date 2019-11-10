0264555d3c8cc8d723067932a6b67f934f9a539c
Gabriel <gabrielstrzal@gmail.com>
2019-11-10 09:32:22 -0400

    added player animation and change of direction

--- TinyLittleAlien/Assets/Scripts/Player.cs
+++ TinyLittleAlien/Assets/Scripts/Player.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    private float input;
 
     Rigidbody2D rb;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (input != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+        //player facing right (rotate the player)
+        if (input > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (input < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //player input
-        float input = Input.GetAxisRaw("Horizontal");
+        input = Input.GetAxisRaw("Horizontal");
         
         //moving the player
         rb.velocity = new Vector2(input * speed, rb.velocity.y);
